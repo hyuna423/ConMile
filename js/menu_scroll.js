0900872da755d8791dedbbd9ff1daadd99ee566c
somang1206 <dlthakd1206@naver.com>
2022-11-24 10:29:12 +0900

    sm_main_code

--- js/menu_scroll.js
+++ js/menu_scroll.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
 $(".scroll").click(function(event){
 
-event.preventDefault();
+/* event.preventDefault(); */
 
 $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 
@@ -12,3 +12,4 @@ $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 
 
 //메뉴 클릭시 자연스러운 스크롤 이동
+
